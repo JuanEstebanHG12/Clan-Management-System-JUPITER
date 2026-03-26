2941db7715ede6871ada71995c99914435162cae
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-03-26 13:55:59 -0500

    User model

--- app/src/models/User.java
+++ app/src/models/User.java
@@ -1,1 +1,31 @@
 //Place where you create the class of User.
+package models;
+
+import enums.Role;
+
+//Place where you create the class of User.
+public class User {
+    private static int counter = 1;
+
+    private int id;
+    private String username;
+    private String password;
+    private Role role;
+
+    public User(String username, String password, Role role) {
+        this.id = counter++;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId()           { return id; }
+    public String getUsername()  { return username; }
+    public String getPassword()  { return password; }
+    public Role getRole()        { return role; }
+
+    @Override
+    public String toString() {
+        return "id->[" + id + "] " + username + " (" + role + ")";
+    }
+}
