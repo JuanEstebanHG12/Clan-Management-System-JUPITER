84bd3e808dc5b3c209b1ae15cee5a6e682a382c8
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-03-31 13:43:11 -0500

    feat:Track failed attempts after 3 tries

--- app/com/jupiter/src/controllers/UserController.java
+++ app/com/jupiter/src/controllers/UserController.java
@@ -6,18 +6,30 @@ import services.UserServices;
 import java.util.Scanner;
 
 public class UserController {
-    public static void LoginController(){
+    public static void LoginController() throws Exception {
         var scanner = new Scanner(System.in);
         System.out.println("=== SISTEMA DE LOGIN JUPITER ===");
-        // 1. CAPTURAR DATOS
-        System.out.print("Email: ");
-        String email = scanner.nextLine();
 
-        System.out.print("Password: ");
-        String password = scanner.nextLine();
 
         // 2. DELEGAR AL SERVICE (No al Repository directamente)
-        User loggedUser = UserServices.LoginService(email, password);
+        User loggedUser = null;
+
+        for (var i = 0; i<3; i++){
+            try {
+                // 1. CAPTURAR DATOS
+                System.out.print("Email: ");
+                String email = scanner.nextLine();
+
+                System.out.print("Password: ");
+                String password = scanner.nextLine();
+                loggedUser = UserServices.LoginService(email, password);
+                if (loggedUser != null) {
+                    break;
+                }
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
 
         // 3. DECIDIR QUÉ MOSTRAR
         if (loggedUser != null) {
--- app/com/jupiter/src/services/UserServices.java
+++ app/com/jupiter/src/services/UserServices.java
@@ -4,12 +4,16 @@ import models.User;
 import repository.UserRepository;
 
 public class UserServices {
-    public static User LoginService(String email, String password){
+    public static User LoginService(String email, String password) throws Exception {
         User user = UserRepository.findByEmail(email);
+        if (user == null) {
+            throw new Exception("Some of the fields do not match");
+        }
 
-        if (user != null && user.getPassword().trim().equals(password)){
+        if (user.getPassword().trim().equals(password)) {
             return user;
         }
+
         return null;
     }
 }
