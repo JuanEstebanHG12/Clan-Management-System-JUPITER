f8bdefe092e91c5c04f10bfcebc5d18f79707e9b
SixOrca00-collab <sixorca00@gmail.com>
2026-03-31 09:46:23 -0500

    refactor(Main):Vacie el Main ya que lo use para testear y olvide limpiarlo

--- app/com/jupiter/src/Main.java
+++ app/com/jupiter/src/Main.java
@@ -1,14 +1,2 @@
-package com.jupiter.src;
+//The main app where excute the main code.
 
-import com.jupiter.src.repository.AdminRepository;
-
-public class Main {
-    public static void main(String[] args) {
-
-        AdminRepository adminRepo = new AdminRepository();
-
-
-        System.out.println("--- Probando importación de CSV ---");
-        adminRepo.getAllUsers();
-    }
-}
