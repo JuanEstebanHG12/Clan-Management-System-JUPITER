f65f3ba85b3d1774c6fceb02420cc1986a756e80
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-03-30 23:44:34 -0500

    feat(models): add Admin model extended User model

--- app/com/jupiter/src/models/Admin.java
+++ app/com/jupiter/src/models/Admin.java
@@ -1,1 +1,11 @@
+package models;
+
+import models.User;
+import models.enums.Role;
+
 //Place where you create the class of Admin.
+public class Admin extends User {
+    public Admin(String username, String password, Role role){
+        super(username, password, role);
+    }
+}
